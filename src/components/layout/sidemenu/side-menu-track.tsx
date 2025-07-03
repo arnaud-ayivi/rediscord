694ef49ec855b38c650721b9b4184772feb631b6
Arnaud Ayivi <rae.arnaud@gmail.com>
2025-07-03 16:32:58 -0700

    side menu fix to avoid console errors

--- src/components/layout/sidemenu/side-menu-track.tsx
+++ src/components/layout/sidemenu/side-menu-track.tsx
@@ -12,33 +12,18 @@ type SideMenuTrackProps = {
     servers: ListedServer[];
 };
 
-type ServerMenuItemProps = {
-    server: ListedServer;
-    isActive: boolean;
-} & React.ComponentProps<typeof SideMenuItem>;
-
-const ServerMenuItem = ({
-                            server,
-                            isActive,
-                            ...props
-                        }: Omit<ServerMenuItemProps, "tooltipContent">) => {
-    return (
-        <SideMenuItem
-            isActive={isActive}
-            notificationCount={server.messages}
-            tooltipContent={<div className="font-semibold">{server.name}</div>}
-            className="mx-auto my-2"
-            image={{
-                url: server.photo,
-                alt: server.name,
-            }}
-            {...props}
-        />
-    );
-};
-
 export default function SideMenuTrack({ servers }: SideMenuTrackProps) {
     const [active, setActive] = useState<string>("default");
+
+    const handleServerClick = (serverId: string) => {
+        // Only allow navigation to the implemented server
+        if (serverId !== "maga-forever") {
+            // For other servers, just show as active but don't navigate
+            setActive(serverId);
+            return;
+        }
+        setActive(serverId);
+    };
 
     return (
         <>
@@ -62,35 +47,63 @@ export default function SideMenuTrack({ servers }: SideMenuTrackProps) {
 
                 <Divider className="w-8" />
 
-                {/* MAGA Forever Server */}
+                {/* Server - Functional */}
                 <SideMenuItem
                     href="/servers/maga-forever/announcements"
-                    onClick={() => setActive("maga-forever")}
-                    tooltipContent={<div className="font-semibold">MAGA Forever</div>}
+                    onClick={() => handleServerClick("maga-forever")}
+                    tooltipContent={<div className="font-semibold">MAGA Community</div>}
                     notificationCount={3}
                     className="mx-auto my-2"
                     isActive={active === "maga-forever"}
                     image={{
                         url: "/img123.png",
-                        alt: "MAGA Forever",
+                        alt: "MAGA",
                     }}
                 />
 
                 <Divider className="w-8" />
 
                 {/*
-          List of other servers
+          List of other servers - Visual only, no navigation
         */}
                 {servers?.map((server) => (
-                    <ServerMenuItem
-                        href={`/servers/${server.id}`}
+                    <button
                         key={server.id}
-                        server={server}
-                        isActive={active === server.id}
-                        onClick={() => {
-                            setActive(server.id);
-                        }}
-                    />
+                        onClick={() => handleServerClick(server.id)}
+                        className={clsx(
+                            "group relative block h-12 w-12 bg-foreground bg-cover transition-all hover:shadow-xl mx-auto my-2",
+                            "focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2",
+                            "focus-visible:ring-offset-background active:translate-y-[1px]",
+                            active === server.id ? "rounded-[15px]" : "rounded-[100%] hover:rounded-[15px]",
+                        )}
+                        title={server.name}
+                    >
+                        {/* Side indicator */}
+                        <div
+                            className={clsx(
+                                "absolute -left-4 w-[9px] rounded-lg bg-white",
+                                "transition-all group-hover:scale-100",
+                                active === server.id ? "bottom-1 top-1" : "top-1/2 -mt-3 h-6 scale-0",
+                            )}
+                        />
+
+                        {/* Notification badge */}
+                        {server.messages && (
+                            <div className="absolute -bottom-1 -right-1 min-w-[25px] text-center rounded-full px-[4px] py-[0px] text-[11px] font-bold border-4 border-background bg-red-500 text-white">
+                                {server.messages > 99 ? "99+" : server.messages}
+                            </div>
+                        )}
+
+                        {/* Server image */}
+                        <img
+                            src={server.photo}
+                            alt={server.name}
+                            className={clsx(
+                                "absolute inset-0 w-full h-full object-cover transition-all",
+                                active === server.id ? "rounded-[15px]" : "rounded-[100%] hover:rounded-[15px]",
+                            )}
+                        />
+                    </button>
                 ))}
             </TooltipProvider>
         </>
